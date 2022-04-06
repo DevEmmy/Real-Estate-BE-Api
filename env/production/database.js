e55c84dc6fbb2aab1894482076cebb8c865ad48a
Emmanuel Olaosebikan <eolaosebikan60@gmail.com>
2022-04-06 20:09:22 +0100

    Update database.js

--- env/production/database.js
+++ env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(postgres://dgnmompoamdtvt:6ef863ed291dd7bf0507b22bfffbd88dc58e6ef06325347958e9108374f9076f@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d3ni1fnh3b4u3b);
+const config = parse("postgres://dgnmompoamdtvt:6ef863ed291dd7bf0507b22bfffbd88dc58e6ef06325347958e9108374f9076f@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d3ni1fnh3b4u3b");
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
