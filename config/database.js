3b4942009eff03a98a3c81b9139163f9e3eae572
DevEmmy <eolaosebikan60@gmail.com>
2022-04-07 06:38:54 +0100

    Fixed DB issue

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
-const path = require('path');
-
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
